38d62c002b2b8599075199a1fee9ca8fa922d81e
tyy-dev <tyy53841@gmail.com>
2024-06-25 16:34:25 +0200

    remove unnecessary suppressions

--- consoletestproject/GlobalSuppressions.cs
+++ consoletestproject/GlobalSuppressions.cs
@@ -1,4 +1,4 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
@@ -6,11 +6,5 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible", Justification = "<Pending>", Scope = "member", Target = "~F:consoletestproject.Menus.MenuService.menuHistory")]
-[assembly: SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible", Justification = "<Pending>", Scope = "member", Target = "~F:consoletestproject.Menus.MenuConfig.displayCurrentMenuAsConsoleTitle")]
-[assembly: SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible", Justification = "<Pending>", Scope = "member", Target = "~F:consoletestproject.Menus.MenuConfig.clearConsoleAfterVisit")]
-[assembly: SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible", Justification = "<Pending>", Scope = "member", Target = "~F:consoletestproject.Menus.MenuConfig.shouldMarkDebugOptions")]
-[assembly: SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible", Justification = "<Pending>", Scope = "member", Target = "~F:consoletestproject.Menus.MenuConfig.shouldShowDebugOptions")]
-[assembly: SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible", Justification = "<Pending>", Scope = "member", Target = "~F:consoletestproject.Menus.MenuConfig.standardInputDelimiter")]
 [assembly: SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible", Justification = "<Pending>", Scope = "member", Target = "~F:consoletestproject.Menus.MenuService.currentMenu")]
 [assembly: SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible", Justification = "<Pending>", Scope = "member", Target = "~F:consoletestproject.Menus.MenuService.menus")]
-[assembly: SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible", Justification = "<Pending>", Scope = "member", Target = "~F:consoletestproject.Menus.MenuConfig.clearConsoleAfterExecute")]
